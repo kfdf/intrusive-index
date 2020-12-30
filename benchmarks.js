86fa92882dda3488f2300f95051f77eedaddc5e6
KhanFusion <khanfusion@mail.ru>
2020-12-30 19:11:39 +0300

    fixed benchmarks

--- benchmarks.js
+++ benchmarks.js
@@ -11,12 +11,18 @@ for (let i = 0; i < 10; i++) {
     let rator = ii.enumerate(i, i + 1)
     while (rator.moveNext()) {
       sum += rator.current.value
-    }    
+    }  
+    let rator2 = ii.enumerate(a => a.value - i)
+    while (rator2.moveNext()) {
+      sum += rator2.current.value
+    }       
     sum += ii.getAt(i).value
     sum += ii.get({ value: i }).value    
     sum += ii.get(n => n.value - i).value    
+    sum += ii.findRange(n => n.value - i, 'any').value    
   }  
 }
+
 
 function Row(value) { 
   this[IIA.l] = null 
@@ -26,15 +32,28 @@ function Row(value) {
 }
 function enumerateInChunks(ii, chunkSize) {
   let sum = 0
-  console.log('enumerating in', chunkSize, 'sized chunks...')
+  console.log('enumerating values in', chunkSize, 'sized chunks...')
   let start = Date.now()
+  sum = 0
   for (let i = 0, len = ii.size; i < len; i += chunkSize) {
-    let rator = ii.enumerate(i, i + chunkSize)
+    let pred = a => a.value < i ? -1: a.value < i + chunkSize ? 0 : 1
+    let rator = ii.enumerate(pred)
+    while (rator.moveNext()) {
+      sum += rator.current.value
+    } 
+  }
+  console.log(Date.now() - start, 'where', sum)
+  start = Date.now()  
+  sum = 0
+  for (let i = 0, len = ii.size; i < len; i += chunkSize) {
+    let pred = a => a.value < i ? -1: a.value < i + chunkSize ? 0 : 1
+    let { start, end } = ii.findRange(pred)
+    let rator = ii.enumerate(start, end)
     while (rator.moveNext()) {
       sum += rator.current.value
     }      
-  }
-  console.log(Date.now() - start, 'sum =', sum)
+  }  
+  console.log(Date.now() - start,  'findRange + range', sum)
 }
 function run(values) {
   console.log('creating index...')
@@ -44,13 +63,34 @@ function run(values) {
     ii.add(new Row(values[i]))
   }
   console.log(Date.now() - start)
-  console.log('enumerating by index...')
+  console.log('reading values one-by-one...')
   start = Date.now()
   let sum = 0
   for (let i = 0, len = ii.size; i < len; i++) {
-    sum += ii.getAt(i).value
+    sum += ii.getAt(values[i]).value
   }
-  console.log(Date.now() - start, 'sum =', sum)
+  console.log(Date.now() - start, 'getAt', sum)
+  start = Date.now()
+  sum = 0
+  for (let i = 0, len = values.length; i < len; i++) {
+    let value = values[i]
+    sum += ii.get(a => a.value - value).value
+  }
+  console.log(Date.now() - start, 'get where', sum)  
+  start = Date.now()
+  sum = 0
+  for (let i = 0, len = values.length; i < len; i++) {
+    let value = values[i]
+    sum += ii.findRange(a => a.value - value, 'any').first.value
+  }
+  console.log(Date.now() - start, 'findRange', sum)  
+  start = Date.now()
+  sum = 0
+  for (let i = 0, len = values.length; i < len; i++) {
+    let value = values[i]
+    sum += ii.get({ value }).value
+  }
+  console.log(Date.now() - start, 'getValue', sum)   
   enumerateInChunks(ii, 1)
   enumerateInChunks(ii, 2)
   enumerateInChunks(ii, 3)
